f757671b79d1887441977b46499bf5e38d02ce12
JoyceLA <joycseleneanaid@gmail.com>
2014-11-07 08:22:04 -0600

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+pip install -r requirements.txt
+autopep8 -ir .
+flake8 --max-complexity=6 --exclude=*.txt,*.md --max-line-length=200 .
+lettuce AceptanceTest
+cd UnitTest
+python TestCase.py
+coverage run --branch TestCase.py
+coverage report -m
